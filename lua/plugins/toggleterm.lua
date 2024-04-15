e74ca6603963804d10069b25bd0fbb6f9640f8f8
Roger Filomeno <rpfilomeno@gmail.com>
2024-04-15 17:40:16 +0800

    simplify all config to use lazy opts

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,36 @@
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+
 return {
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
+  { 
+    "akinsho/toggleterm.nvim", 
+    version = "*", 
+    config = true,
+    opts={
+      size = 40,
+      open_mapping = [[<C-\>]],
+      hide_numbers = true,
+      shade_terminals = true,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      close_on_exit = true,
+      direction = 'float',
+      float_opts = {
+        winblend = 3,
+        border = "curve",
+        title_pos = 'left'
+      }
+    }
+  },
 }
